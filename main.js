15a3d923b5ec9f742b75921a240e18cf058c9e7b
nvmRegi <cabael.regine@gmail.com>
2022-03-24 14:44:54 +0100

    Gioco dei Dadi

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let mailUtente = prompt("Inserire la propria email: ");
+mailUtente = mailUtente.toLowerCase();
 
 let listaEmail = ["matteo@gmail.com", "fabio@gmail.com", "pietro@gmail.com", "anna@gmail.com"];
 
@@ -13,3 +14,31 @@ do{
         i++;
     }
 }while(bool || i < listaEmail.length)
+
+if(bool){
+    console.log("L'utente non può accedere al Gioco dei Dadi");
+}else{
+
+    //Funzione per generare un numero random da 1 a 6
+    function generatoreNumeri (min, max) { // min and max included 
+        return Math.floor(Math.random() * (max - min + 1) + min)
+      }
+
+    //numero random giocatore
+    const numRandom = generatoreNumeri(1, 6);
+    console.log("Il tuo numero è");
+    console.log(numRandom);
+
+    //numero random computer
+    const numPC = generatoreNumeri(1, 6);
+    console.log("Il numero del tuo avversario è");
+    console.log(numPC);
+
+    if(numRandom > numPC ){
+        console.log("HAI VINTO!");
+    } else if(numRandom < numPC){
+        console.log("HAI PERSO!");
+    } else{
+        console.log("PAREGGIO!");
+    }
+}
