be8e1d89abb8d7cc8c3c9a13d6368679b8306f04
nvmRegi <cabael.regine@gmail.com>
2022-03-24 16:38:09 +0100

    CicloGiocata

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ do{
     }else{
         i++;
     }
-}while(bool && i < listaEmail.length)*/
+}while(bool && i < listaEmail.length);*/
 
 
 //Verifica con for
@@ -35,30 +35,58 @@ for(let i = 0; i < listaEmail.length && bool; i++){
     }
 }
 
-if(bool){
-    console.log("L'utente non può accedere al Gioco dei Dadi.");
-}else{
+let PlayAgain = true;
 
-    //Funzione per generare un numero random da 1 a 6
-    function generatoreNumeri (min, max) { // min and max included 
-        return Math.floor(Math.random() * (max - min + 1) + min)
-      }
+do{
+    if(bool){
+        console.log("L'utente non può accedere al Gioco dei Dadi.");
 
-    //numero random giocatore
-    const numRandom = generatoreNumeri(1, 6);
-    console.log("Il tuo numero è");
-    console.log(numRandom);
+        let risposta = prompt("Vuoi inserire l'Email nella lista dei giocatori?(si/no)");
+        if(risposta == "si"){
+            listaEmail.push(mailUtente);
 
-    //numero random computer
-    const numPC = generatoreNumeri(1, 6);
-    console.log("Il numero del tuo avversario è");
-    console.log(numPC);
+            for(let i = 0; i < listaEmail.length; i++){
+                console.log(listaEmail[i]);
+            }
 
-    if(numRandom > numPC ){
-        console.log("HAI VINTO!");
-    } else if(numRandom < numPC){
-        console.log("HAI PERSO!");
-    } else{
-        console.log("PAREGGIO!");
+            bool = false;
+        } else{
+            console.log("Giocheremo una prossima volta :c");
+            
+            bool = false;
+            PlayAgain = false;
+        }
+
+    }else{
+
+        //Funzione per generare un numero random da 1 a 6
+        function generatoreNumeri (min, max) { // min and max included 
+            return Math.floor(Math.random() * (max - min + 1) + min)
+        }
+
+        //numero random giocatore
+        const numRandom = generatoreNumeri(1, 6);
+        console.log("Il tuo numero è");
+        console.log(numRandom);
+
+        //numero random computer
+        const numPC = generatoreNumeri(1, 6);
+        console.log("Il numero del tuo avversario è");
+        console.log(numPC);
+
+        if(numRandom > numPC ){
+            console.log("HAI VINTO!");
+        } else if(numRandom < numPC){
+            console.log("HAI PERSO!");
+        } else{
+            console.log("PAREGGIO!");
+        }
+
+        risposta = prompt("Vuoi giocare ancora?(si/no)");
+        if(risposta == "no"){
+            PlayAgain = false;
+        }
     }
-}
+} while(PlayAgain);
+
+console.log("Arrivederci!");
